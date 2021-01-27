702314a6c6075e7080e14ce3d8a767433f107832
rsoric <robert.soric@gmail.com>
2021-01-27 09:36:13 +0100

    Add project files.

---
+++ RUAP-LV6_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAP_LV6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
